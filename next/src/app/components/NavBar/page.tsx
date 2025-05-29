d3d8ab1f97c788eec92cb86f11f381996127e2be
MikeOhtani <yaowenchen998@gmail.com>
2025-05-30 03:50:10 +0800

    update navbar with btr UI

--- next/src/app/components/NavBar/page.tsx
+++ next/src/app/components/NavBar/page.tsx
@@ -2,35 +2,37 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 export default function NavBar() {
+
+  const pill = "flex items-center h-6 rounded-full p-4"
+
   return (
-    <header className = 'flex w-full h-16 bg-slate-50 shadow-2xl flex-row flex-nowrap justify-between align'>
-    {/*Logo*/}
-    <Image  
-      src="/NearUliveS@NUS.jpg"
-      alt="NearUliveS Logo"
-      width={300}
-      height={10}
-      className='
-      pl-10 py-4 pr-4'
-      />
-    {/*search bar*/}
-    <form>
-      <input type='text' 
-      value={""} 
-      placeholder='Search' 
-      className='
-      w-100
-      bg-gray-100
-      rounded-full
-      p-4 
-      focus:outline-amber-200
-      focus:ring-amber-200'
-      aria-label='Search query'>
-      </input>
+    <header className = 'relative flex w-full h-16 bg-slate-50 shadow-2xl p-4 justify-between items-center'>
+      {/*logo*/}
+      <div className={`${pill} flex-shrink-0 bg-yellow-100`}>
+        <Image 
+        src ='/app_logo.svg'
+        alt ='AppLogo'
+        width={200}
+        height={100}
+        />
+      </div>
+      {/*search bar*/}
+      <form className={`${pill} bg-gray-100 w-1/3 mx-6`}>
+        <input 
+        type='text' 
+        value={""} 
+        placeholder='Search' 
+        className='
+        w-full
+        rounded-full
+        focus:outline-amber-200
+        focus:ring-amber-200'
+        aria-label='Search query'>
+        </input>
       </form>
     {/*user profile*/}
     <Image
-      src="/public/globe.svg"
+      src="/globe.svg"
       alt='User avatar'
       className='rounded-full'
       width={10}
