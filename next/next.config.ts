a4493d02feb05b478c9826bca310302ce0c69033
XZHHHHHH <xuzihaotm66@gmail.com>
2025-05-31 03:28:35 +0800

    conig update

--- next/next.config.ts
+++ next/next.config.ts
@@ -1,12 +1,3 @@
 import type { NextConfig } from "next";
-const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-    return config
-  },
-}
+const nextConfig = {}
 export default nextConfig;
