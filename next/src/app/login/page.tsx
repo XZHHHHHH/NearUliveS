9686513cabb0c7817ef380f16b0f5e451c602c44
XZHHHHHH <xuzihaotm66@gmail.com>
2025-06-10 00:11:34 +0800

    login update

--- next/src/app/login/page.tsx
+++ next/src/app/login/page.tsx
@@ -24,14 +24,14 @@ export default function Login() {
                     style={{marginTop:'120px'}}
                 />
             </div>
-                <h1 className="welcomeText text-white text-center font-mono text-large md:text-xl lg:text-2xl mb-4">
+                <h1 className="welcomeText text-white text-center text-large md:text-xl lg:text-2xl mb-4">
                     Welcome to <span className=" text-orange-500">N</span>ear
                     <span className="text-orange-500">U</span>live<span className="text-orange-500">S</span>
                 </h1>
-                <h2 className=" text-white text-center font-mono text-xs md:text-l lg:text-xl">
+                <h2 className=" text-white text-center text-xs md:text-l lg:text-xl">
                     Log in to know more about
                 </h2>
-                <h3 className=" text-white text-center font-mono text-xs md:text-l lg:text-xl"> 
+                <h3 className=" text-white text-center text-xs md:text-l lg:text-xl"> 
                     wonderful events in <span className="text-orange-500">NUS</span>
                 </h3>
                 <div className="flex justify-center items-center">
