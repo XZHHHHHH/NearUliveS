994b05962891a9b54409d08d7c7efa2f67ef29bf
MikeOhtani <yaowenchen998@gmail.com>
2025-06-28 01:40:22 +0800

    add homepage layout

--- next/src/app/(home-components)/layout.tsx
+++ next/src/app/(home-components)/layout.tsx
@@ -5,24 +5,24 @@ import { SideBar } from '../components/SideBar/page';
 
 export default function RootLayout({children}: {children: ReactNode}) {
   return (
-      <body>
-        <header className="fixed w-full flex flex-col">
-          {/* NavBar at the top*/}
-          <NavBar />
-        </header>
-          {/* section for side bar & other dynamic page contents
-          (e.g home and notifications*/}
-        <div className="flex flex-row h-screen bg-yellow-50">
-          <aside className="fixed top-30 w-60">
-            {/* Side bar at the left*/}
-            <SideBar />
-          </aside>
+    <div>
+      <header className="fixed w-full flex flex-col">
+        {/* NavBar at the top*/}
+        <NavBar />
+      </header>
+        {/* section for side bar & other dynamic page contents
+        (e.g home and notifications*/}
+      <div className="flex flex-row h-screen bg-yellow-50">
+        <aside className="fixed top-30 w-60">
+          {/* Side bar at the left*/}
+          <SideBar />
+        </aside>
 
-          {/* Dynamic page contents:
-          specific syntax: {children} indicates the page content*/}
-          <main className="ml-60 mt-30 flex-1 overflow-y-auto">{children}
-          </main>
-        </div>
-      </body>
+        {/* Dynamic page contents:
+        specific syntax: {children} indicates the page content*/}
+        <main className="ml-60 mt-30 flex-1 overflow-y-auto">{children}
+        </main>
+      </div>
+    </div>
   );
 }
--- next/src/app/lib/controller/authController.ts
+++ next/src/app/lib/controller/authController.ts
@@ -1,4 +1,4 @@
-import { PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 import bcrypt from 'bcrypt';
 
@@ -8,52 +8,55 @@ const prisma = new PrismaClient();
 // await ensures them to run in order.
 // async is recommended for I/O(Input/Output) to perform efficiently. 
 
-// req: NextRequest represents te incoming HTTP request
+// req: NextRequest represents the incoming HTTP request
 async function register(req: NextRequest) { 
-    // read the JSON format request and extract is to JavaScript.
-    const {email, password, confirmPassword} = await req.json(); 
+    // read the JSON format request and extract it to JavaScript.
+    const { email, password, confirmPassword } = await req.json(); 
 
-// findUnique is a Prisma method to search for a unique record
-// 409 Conflict  is an official HTTP status code. It means: The request could not be completed because of a conflict with the current state of the resource
-const exists = await prisma.user.findUnique({where:{ email }});
-if (exists) {
-    return NextResponse.json({ error: "Email already exists"}, {status: 409});
-}
+    // findUnique is a Prisma method to search for a unique record
+    // 409 Conflict is an official HTTP status code. It means: 
+    // The request could not be completed because of a conflict with the current state of the resource
+    const exists = await prisma.user.findUnique({ where: { email } });
+    if (exists) {
+        return NextResponse.json({ error: "Email already exists" }, { status: 409 });
+    }
 
-if(confirmPassword != password) {
-    return NextResponse.json({ error: "Passwords do not match"}, {status: 400})
-}
+    if (confirmPassword !== password) {
+        return NextResponse.json({ error: "Passwords do not match" }, { status: 400 });
+    }
 
-// bcrypt.hash(password: string, saltRounds: number)
-// saltRounds stands for how secure the hasing process is
-const hashPassword = await bcrypt.hash(password, 10);
+    // bcrypt.hash(password: string, saltRounds: number)
+    // saltRounds stands for how secure the hashing process is
+    const hashPassword = await bcrypt.hash(password, 10);
 
-const user = await prisma.user.create({
-    data: { email, password: hashPassword},
-});
+    const user = await prisma.user.create({
+        data: { email, password: hashPassword },
+    });
 
-return NextResponse.json({ 
-    message: "User registered",
-    user: {email: user.email},
-});
+    return NextResponse.json({ 
+        message: "User registered",
+        user: { email: user.email },
+    });
 } 
 
-
+// this function returns login result only, cookie should be set in the API route
 async function login(email: string, password: string) {
-        const user = await prisma.user.findUnique({ where: {email}})
-        if (!user){
-            return {success: false, error: "Invalid email", field: "email", status: 401};
-        }
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+        return { success: false, error: "Invalid email", field: "email", status: 401 };
+    }
 
-        const match = await bcrypt.compare(password, user.password);
-        if(!match) {
-            return {success: false, error: "Wrong password", field: "password", status: 401}
-        } else {
-            return {success: true, message: "Login successful", status: 200,
-            user: { email: user.email}};
-        }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+        return { success: false, error: "Wrong password", field: "password", status: 401 };
+    }
+
+    return {
+        success: true,
+        message: "Login successful",
+        status: 200,
+        user: { email: user.email },
+    };
 }
 
-export{ register, login };
-
-// async function publish() {}
+export { register, login };
