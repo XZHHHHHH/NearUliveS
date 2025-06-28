e98621c0ccad75a8bc888d0fcf6235890d451007
MikeOhtani <yaowenchen998@gmail.com>
2025-06-28 18:35:46 +0800

    userprofile page refine

---
+++ next/src/app/(home-components)/userprofile/page.tsx
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client';
+import { cookies } from 'next/headers';
+import FullUserProfile from '../../components/FullUserProfile/page'; 
+
+const prisma = new PrismaClient();
+
+export default async function UserProfilePage() {
+  //Use await in order to wait for the cookies() to return the promise before applying get() on it
+  const cookieStore = await cookies(); 
+  const userEmail = cookieStore.get("userEmail")?.value;
+
+  // not useremail found, then shown login failure message
+  if (!userEmail) return <p>Not logged in</p>;
+
+  const user = await prisma.user.findUnique({
+    where: { email: userEmail },
+    include: { posts: true, profile: true },
+  });
+
+  if (!user || !user.profile) return <p>User profile not found.</p>;
+
+  return <FullUserProfile profile={user.profile} posts={user.posts} />;
+}
